eb290e348a09181f90e774c92e35acd4bcfb12c4
jeddaa <fathiato@gmail.com>
2023-03-08 10:55:58 +0100

    making lint changes

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const mainMenu = document.querySelector(".mainMenu");
-const closeMenu = document.querySelector(".closeMenu");
-const openMenu = document.querySelector(".openMenu");
-const navLInk = document.querySelectorAll(".nav-link");
-
-openMenu.addEventListener("click", show);
-closeMenu.addEventListener("click", close);
-navLInk.forEach((n) => n.addEventListener("click", close));
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const navLInk = document.querySelectorAll('.nav-link');
 
 function show() {
-  mainMenu.style.display = "flex";
-  mainMenu.style.top = "0";
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
 }
 
 function close() {
-  mainMenu.style.top = "-100%";
+  mainMenu.style.top = '-100%';
 }
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+navLInk.forEach((n) => n.addEventListener('click', close));
