43e38d0f1d09d50b71acf7f9434b474eaa533e0c
jeddaa <fathiato@gmail.com>
2023-03-08 10:37:05 +0100

    making changes

---
+++ index.js
@@ -0,0 +1,17 @@
+const mainMenu = document.querySelector(".mainMenu");
+const closeMenu = document.querySelector(".closeMenu");
+const openMenu = document.querySelector(".openMenu");
+const navLInk = document.querySelectorAll(".nav-link");
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+navLInk.forEach((n) => n.addEventListener("click", close));
+
+function show() {
+  mainMenu.style.display = "flex";
+  mainMenu.style.top = "0";
+}
+
+function close() {
+  mainMenu.style.top = "-100%";
+}
